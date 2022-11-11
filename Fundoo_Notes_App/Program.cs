5656ba0a502ff6108297203f18568dd3ee50ede9
Sandeep singh <sandy180388@gmail.com>
2022-11-11 15:41:46 +0530

    [sandeep] decryption is added in UC_9

--- Fundoo_Notes_App/Program.cs
+++ Fundoo_Notes_App/Program.cs
@@ -2,17 +2,21 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Fundoo_Notes_App
+namespace Fundoo_App
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var logPath = Path.Combine(Directory.GetCurrentDirectory(), "Logs");
+            NLog.GlobalDiagnosticsContext.Set("LogDirectory", logPath);
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -21,6 +25,9 @@ namespace Fundoo_Notes_App
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }).ConfigureLogging(opt => {
+                    opt.ClearProviders();
+                    opt.SetMinimumLevel(LogLevel.Trace);
+                }).UseNLog();
     }
 }
